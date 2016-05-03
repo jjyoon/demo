363785adee85caf8c88404dde41da74f90095e60
jjyoon <jim.yoon93@gmail.com>
2016-05-03 00:49:00 -0500

    05/3/16 initial commit

---
+++ project-pages/pulseband.php
@@ -0,0 +1,97 @@
+<?php ?>
+
+
+   <header>
+    <div class="exit_contain">
+        <div class="exit"></div>
+    </div>
+    <figure>
+        <img src="img/pulseband/pb_screens.png" alt="">
+    </figure>
+    <h2>Pulseband</h2>
+    <div class="line"></div>
+    <h3>
+                            Client: Designation Labs <br>
+                            Role: UI Design, Front-End Dev
+                        </h3>
+</header>
+
+<div class="content margins_large">
+    <h2>The Challenge</h2>
+
+    <p>
+        DESIGNATION Labs gave my team a project to design a mobile companion app for their product, the Pulseband, a piece of wearable health technology that monitors a user’s heart statistics and condition. Through domain research and interviews, our team concluded that a lot of people don’t have clear, fast, or efficient ways to monitor the heart health and progress of themselves, loved ones, or patients.
+    </p>
+
+    <figure class="img_normal">
+        <img src="img/pulseband/sketch1.jpg" alt="">
+        <figcaption>Ideating different dashboards for the interface</figcaption>
+    </figure>
+
+    <h2>The Solution</h2>
+
+    <p>
+        The UX research showed that users want an app that utilizes the data in a meaningful way, provides motivation, and is encouraging. We used that information to design an app that was easily understandable, provided actionable recommendations, and gave encouragement via an online social circle.
+    </p>
+    <p>
+        Our main goals were to make the UI design help the user identify their heart’s current condition as well as what they could do to improve their health, along with making sure that the UI design followed current aesthetic trends. For the front-end development, this pertained more to the marketing website, which I designed and coded. The main goals for the website were to make the product appealing and to showcase the companion app and its features.
+    </p>
+
+    <p>
+        Below are some exploratory style tiles, each trying to encompass a different type of aesthetic.
+    </p>
+
+    <div class="slideshow_contain margins_fix">
+        <ul class="slideshow margins_large">
+            <li>
+                <figure>
+                    <img src="img/pulseband/pb_tile1.png" alt="">
+                    <figcaption>Style Tile #1</figcaption>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <img src="img/pulseband/pb_tile2.png" alt="">
+                    <figcaption>Style Tile #2</figcaption>
+                </figure>
+            </li>
+        </ul>
+    </div>
+
+    <p>
+        Style #1 was chosen to be applied to the companion app. This approach was to use accent colors on a white background to emphasize the heart’s current status or highlight different areas of the app.
+    </p>
+
+    <figure class="full_span margins_large margins_fix">
+        <img class="full_width" src="img/pulseband/pb_phones_transp.png" alt="">
+    </figure>
+
+    <p>
+        For the marketing website, the approach was to make it friendly and clean. Below is the style guide of the website along with mock-ups. The aesthetic taken with the design, specifically the color palette, was blue-themed because the color scheme resonated with our logo; we also felt that the color scheme was inviting.
+    </p>
+
+    <figure class="full_span margins_large">
+        <img class="mid_span" src="img/pulseband/pb_styleguide.png" alt="" class="full_width">
+    </figure>
+
+    <figure class="full_span margins_large">
+        <img class="mid_span" src="img/pulseband/landingpage.jpg" alt="">
+    </figure>
+
+    <figure class="full_span margins_large">
+        <img class="full_width" src="img/pulseband/pb_site_screens.png" alt="">
+    </figure>
+
+    <a href="sites/pulseband/index.html" class="button" target="_blank">Visit Live Site</a>
+
+    <div class="bottom_buttons">
+        <div class="return">
+            <img src="img/logo.svg" alt="">
+            <h4>Return to <br>Portfolio</h4>
+        </div>
+    </div>
+
+</div>
+
+
+<?php ?>
